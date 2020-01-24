eff2b5d93e472e487a4efdf43963429d070b3f8a
Sodvadiyavidhi <200429730@student.georgianc.on.ca>
2020-01-23 20:39:08 -0500

    completed lab3

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+
+
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone{
+                    PhoneName ="Samsung Note 9",
+                    MSRP = 3453,
+                    ScreenSize = 2,
+                    DateReleased = new DateTime(2018,08,23),
+                    Manufacturer = "Samsung"
+                },
+                new Phone{
+                    PhoneName ="Samsung Galaxy s9",
+                    MSRP = 4356.65,
+                    ScreenSize = 6,
+                    DateReleased = new DateTime(2018,03,16),
+                    Manufacturer = "Samsung"
+                },
+                new Phone{
+                    PhoneName ="iPhone 11 Pro",
+                    MSRP = 3700.00,
+                    ScreenSize = 2,
+                    DateReleased = new DateTime(2019,09,20),
+                    Manufacturer = "Apple"
+                },
+                new Phone{
+                    PhoneName ="Google pixel 2",
+                    MSRP = 1375,
+                    ScreenSize = 5,
+                    DateReleased = new DateTime(2017,08,10),
+                    Manufacturer = "Google"
+                }
+                };
+
+          
+            return View(phoneList);
+
+
+        }
+        //phoneList.Add("Charger"),
+        //    phoneList.Add("Adapter"),
+        //    phoneList.Add("USB"),
+        //    phoneList.Add("Earplugs")
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            DateReleased = DateTime.Now;
+            PhoneName = "";
+            Manufacturer = "";
+            
+        }
+
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public double MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
